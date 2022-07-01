037e700f2ddb98567d382e1dad1c32127b964844
Fausto Cortez <faustocortez1994@gmail.com>
2022-07-01 00:38:48 -0500

    Make class the connection function to inject to container

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -1,21 +1,10 @@
 import { Request, Response } from "express";
 import { controller, httpGet } from "inversify-express-utils";
-import { connect } from "../database";
 
 @controller("/")
 export class HomeController {
   @httpGet("")
   public index(req: Request, res: Response) {
-    return res.send("Hello world");
-  }
-
-  @httpGet("register")
-  public async getRegisters(req: Request, res: Response) {
-    const conn = await connect();
-    const [registers] = await conn.query(
-      'select * from calls.register where agente=? limit 2;',
-      [7874]
-    )
-    return res.json({ registers });
+    return res.send("Bazinga!");
   }
 }
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,4 +1,5 @@
 
+import { inject, injectable } from 'inversify';
 import dotenv from 'dotenv';
 import { createPool, Pool } from 'mysql2/promise';
 import { TYPES } from '../core/types';
@@ -6,17 +7,35 @@ import Logger from '../services/logger.service';
 import { LogLevel } from '../interfaces/services/logger.interface';
 
 dotenv.config();
+@injectable()
+class Database {
+    private static connection: Pool;
 
-export async function connect() {
-    console.log(LogLevel.DEBUG, `Getting DataSource connection...`);
-    const connection = await createPool({
-        host: 'localhost',
-        user: 'root',
-        password: '9b289300!A',
-        database: 'calls',
-        connectionLimit: 10
-    });
-    console.log(LogLevel.DEBUG, `connected`);
+    private readonly logger: Logger;
 
-    return connection;
+    public constructor(@inject(TYPES.Logger) logger: Logger) {}
+
+    public async getConnection(): Promise<Pool> {
+        this.logger.log(LogLevel.DEBUG, `Getting DataSource connection...`);
+
+        if (Database.connection instanceof Database) return Database.connection;
+        
+        try {
+            Database.connection = createPool({
+                host: process.env.DB_HOST,
+                user: process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+                database: process.env.DB_NAME,
+                connectionLimit: Number(process.env.DB_CONNECTION_LIMIT)
+            });
+        } catch (error) {
+            this.logger.log(LogLevel.DEBUG, 'Cannot establish database connection');
+            this.logger.log(LogLevel.ERROR, error);
+        }
+        
+        this.logger.log(LogLevel.DEBUG, `Connection established`);
+        return Database.connection;
+    }
 }
+
+export default Database;
